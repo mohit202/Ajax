928ec6da61b1fc67a0bc149f1c78070976c00b2e
Mohit Yadav <mohitsryadav@gmail.com>
2020-02-18 14:19:55 +0530

    AjaxAssignment

--- js/ajax.js
+++ js/ajax.js
@@ -1,22 +1,45 @@
 
-var person = [
-    {"name":"rahul","email":"rahul@gmail.com","message":"xysd","date":"06/05/2016"},
-    {"name":"rohit","email":"rohit@rgmail.com","message":"xysd","date":"08/06/2017"},
-    {"name":"deepak","email":"deepak@gmail.com","message":"xysd","date":"04/05/2015"},
-    {"name":"ashish","email":"ashish@gmail.com","message":"xysd","date":"02/05/2014"},  
-    {"name":"arnav","email":"arnav@gmail.com","message":"xysd","date":"06/05/2014"},  
-]
+var person = fetch('https://jsonplaceholder.typicode.com/comments')
+.then(response => response.json())
+.then(json => table(json))
+
 function table(person){
-var table = document.getElementById("mytable")
+var table = document.getElementById("mytable");
+
+//var slideIndex=0;
+
 for(var i=0; i < person.length; i++)
 {
     var row =`<tr>
-    <td> ${person[i].name}</td>
-    <td> ${person[i].email}</td>
-    <td> ${person[i].message}</td>
-    <td> ${person[i].date}</td>
+    <td class="mySlide"> ${person[i].name}</td>
+    <td class="mySlide"> ${person[i].email}</td>
+    <td class="mySlide"> ${person[i].body}</td>
+    <td class="mySlide"> ${person[i].id}</td>
     </tr>`
     table.innerHTML+=row;
+    table.style.display="none";
+    for(i=0; i<person.length; i++)
+    {   
+        table.innerHTML+=row;
+        table.style.display="block";
+    }
 }
 }
-table(person);
+//slidetable
+// slideIndex = 0;
+// showSlide();
+// function showSlide()
+// {
+//     var slide = document.getElementsByClassName("mySlide");
+//     for(i=0; i < slide.length; i++)
+//     {
+//          slide[i].style.display="none";
+//     }
+//      slideIndex++;
+//    // console.log(slide);
+
+//    if(slideIndex > slide.length){slideIndex =1}
+//    slide[slideIndex-1].style.display="block";
+//    setTimeout(showslide,2000);
+// }
+//console.log("the person length is : "+person.length);
