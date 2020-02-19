7ebd3ba9cdf75c1f3e9fbd221ec1cc301f9e5830
Mohit Yadav <mohitsryadav@gmail.com>
2020-02-19 11:18:34 +0530

    change ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,45 +1,30 @@
 
-var person = fetch('https://jsonplaceholder.typicode.com/comments')
+var person = fetch('https://jsonplaceholder.typicode.com')
+//main link https://jsonplaceholder.typicode.com/comments
 .then(response => response.json())
 .then(json => table(json))
-
 function table(person){
 var table = document.getElementById("mytable");
-
+//replace function
+//document.getElementById('').remove();
 //var slideIndex=0;
-
-for(var i=0; i < person.length; i++)
+for(var i=0; i < 5; i++)
 {
     var row =`<tr>
-    <td class="mySlide"> ${person[i].name}</td>
-    <td class="mySlide"> ${person[i].email}</td>
-    <td class="mySlide"> ${person[i].body}</td>
-    <td class="mySlide"> ${person[i].id}</td>
+    <td>${person[i].name}</td>
+    <td>${person[i].email}</td>
+    <td>${person[i].body}</td>
+    <td>${person[i].id}</td>
     </tr>`
-    table.innerHTML+=row;
-    table.style.display="none";
-    for(i=0; i<person.length; i++)
-    {   
-        table.innerHTML+=row;
-        table.style.display="block";
-    }
+    table.innerHTML+=row; 
 }
 }
-//slidetable
-// slideIndex = 0;
-// showSlide();
-// function showSlide()
-// {
-//     var slide = document.getElementsByClassName("mySlide");
-//     for(i=0; i < slide.length; i++)
-//     {
-//          slide[i].style.display="none";
-//     }
-//      slideIndex++;
-//    // console.log(slide);
-
-//    if(slideIndex > slide.length){slideIndex =1}
-//    slide[slideIndex-1].style.display="block";
-//    setTimeout(showslide,2000);
-// }
-//console.log("the person length is : "+person.length);
+function fdata(val){
+    
+    var person = fetch(`https://jsonplaceholder.typicode.com/comments?_start=${val}&_limit=5`)
+    .then(response => response.json())
+    .then(json => table(json))
+   // 
+   
+}
+fdata(10);
